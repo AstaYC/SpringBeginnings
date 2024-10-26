95e4a92a99bad697ba166c74707c9b840b35f9f0
AstaYC PS <baihmohamedamine@gmail.com>
2024-10-26 16:21:14 +0100

    i added the interface injection

--- src/main/java/com/SpringStart/Controller/Main.java
+++ src/main/java/com/SpringStart/Controller/Main.java
@@ -4,12 +4,12 @@ import com.SpringStart.Service.UserService;
 import com.SpringStart.Model.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.SpringStart.Service.Interface.UserServiceInter;
 
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        UserService user = context.getBean("userServiceSetter", UserService.class);
-
-        user.addUser();
+        UserServiceInter UserServiceInter = (UserServiceInter) context.getBean("userServiceInterface");
+        System.out.println("User added: " + UserServiceInter.getUser().getNom()); // Output: "User added: Interface"    }
     }
 }
